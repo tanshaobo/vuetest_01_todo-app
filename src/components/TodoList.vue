3ac4bd5ca3665f61ac7d51e366f41a78d5ed9b48
tanshaobo <18235140339@163.com>
2019-02-11 14:42:55 +0800

    vue-cli应用练习一

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p> Completed Tasks:{{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p> Pending Tasks:{{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-for="(todo,index) in todos" v-bind:todo="todo" v-bind:key="index" v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo"></todo>
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo'
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    },
+    completeTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
